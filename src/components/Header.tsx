9fbead41d637d7b8c918df4c7bdb4388919f170b
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-18 14:25:01 +0400

    Commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,16 +9,29 @@ type THeader = {
 
 
 export default function Header({ Data, H2, H4 }: THeader) {
-    
+    const colorsObject = {
+        Mercury: "#419EBB",
+        Venus: "#EDA249",
+        Earth: "#6D2ED5",
+        Mars: "#D14C32",
+        Jupiter: "#D83A34",
+        Saturn: "#CD5120",
+        Uranus: "#1EC1A2",
+        Neptune: "#2D68F0"
+    }
+
+
+
     return (
-        <header className="w-[100%] items-center justify-center border-solid border-[1px] border-b-[rgba(255,255,255,0.2)]  p-[32px_50px_32px_50px]! flex">
-            <div className="items-center justify-between flex w-[1440px]">   
-                <h1 className={`${H2} text-[2.8rem]!`}>THE PLANETS</h1>
+        <header className="w-[100%] items-center justify-center border-solid border-b-[1px] border-b-[rgba(255,255,255,0.2)]  p-[0_50px_32px_50px]! flex">
+            <div className="items-center justify-between flex w-[1440px]">
+                <h1 className={`${H2} text-[2.8rem]! mt-[30px]!`}>THE PLANETS</h1>
                 <nav>
                     <ul className="flex gap-[32px]">
                         {Data.map((e) => {
                             return <Link key={e.name} to={`/planets/${e.name}`}>
-                                <li className={`${H4} cursor-pointer`}>{e.name.toUpperCase()}</li>
+
+                                <li style={{borderTop: colorsObject[e?.name as keyof typeof colorsObject]}} className={`${H4} pt-[36px]! hover:pt-[32px]! h-[100%]! mb-[24px]! border-t-[4px]! border-hidden hover:border-solid! cursor-pointer`}>{e.name.toUpperCase()}</li>
                             </Link>
                         })}
                     </ul>
--- src/pages/Planet.tsx
+++ src/pages/Planet.tsx
@@ -34,19 +34,21 @@ export default function Planet() {
   return (
     <>
       <div className="flex items-center justify-center flex-col ">
-        <div className="flex w-[1440px] p-[0_100px_0_100px]! justify-between">
+        <div className="flex w-[1440px] max-xl:flex-col p-[0_100px_0_100px]! justify-between">
           <div className="w-[100%]! flex items-center justify-center">
             <div className="relative h-[700px]! min-w-[700px] flex items-center justify-center">
               <img src={states.structure ? planet?.images.internal : planet?.images.planet} className="block" alt="" />
               {states.geology ? <img src={planet?.images.geology} className="absolute bottom-[5%] left-[38.34%] w-[163px]!" alt="" /> : null}
             </div>
           </div>
-          <div className="max-w-[350px] m-[150px_0_0_100px]!">
-            <div className="flex flex-col gap-[30px]">
-              <h1 className={`${H1}`}>{planet?.name}</h1>
-              <p className={`${P}`}>{states.overview ? planet?.overview.content : states.structure ? planet?.structure.content : states.geology ? planet?.geology.content : ""}</p>
-              <h4 className={`${P} text-[rgba(255,255,255,0.5)]! flex gap-[5px]`}>Source : <span className={`${P} font-[700] flex items-center gap-[10px] text-[rgba(255,255,255,0.7)]!`}><a href={states.overview ? planet?.overview.source : states.structure ? planet?.structure.source : states.geology ? planet?.geology.source : ""} className="underline">Wikipedia</a><img src="/images/icon-source.svg" alt="" /></span></h4>
-              <div className="flex flex-col gap-[20px]">
+          <div className="max-w-[350px] max-xl:p-[0_100px_50px_100px]! max-xl:max-w-[100%]!  m-[150px_0_0_100px]! max-xl:m-0!">
+            <div className="flex flex-col gap-[30px] max-xl:justify-between max-xl:flex-row">
+              <div className="flex flex-col gap-[30px] max-xl:max-w-[350px]!">
+                <h1 className={`${H1}`}>{planet?.name}</h1>
+                <p className={`${P}`}>{states.overview ? planet?.overview.content : states.structure ? planet?.structure.content : states.geology ? planet?.geology.content : ""}</p>
+                <h4 className={`${P} text-[rgba(255,255,255,0.5)]! flex gap-[5px]`}>Source : <span className={`${P} font-[700] flex items-center gap-[10px] text-[rgba(255,255,255,0.7)]!`}><a href={states.overview ? planet?.overview.source : states.structure ? planet?.structure.source : states.geology ? planet?.geology.source : ""} className="underline">Wikipedia</a><img src="/images/icon-source.svg" alt="" /></span></h4>
+              </div>
+              <div className="flex flex-col max-xl:min-w-[350px]! gap-[20px]">
                 <button className={`${buttonStyle}`} style={states.overview ? { backgroundColor: planetColor } : undefined} onClick={() => {
                   setState({ structure: false, geology: false, overview: true })
                 }}><span className={`${H3} text-[rgba(255,255,255,0.5)]!`}>01</span>Overview</button>
@@ -61,7 +63,7 @@ export default function Planet() {
           </div>
 
         </div>
-        <div className="flex p-[0_100px_30px_100px]! w-[1440px] items-center justify-between gap-[24px]">
+        <div className="max-xl:w-[1240px]! flex p-[0_100px_30px_100px]! w-[1440px] items-center justify-between gap-[24px]">
           <PlanetInfo title="ROTATION TIME" info={planet?.rotation} />
           <PlanetInfo title="REVOLUTION TIME" info={planet?.revolution} />
           <PlanetInfo title="RADIUS" info={planet?.radius} />
